cb07cc14c85811ff3840ddf5b8c948955b7db0a2
HJSmiley <hyeju518@gmail.com>
2024-08-26 13:45:48 +0900

    build: npm 모듈 재설치

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,6 @@
-const app = require("./app"); // 'app.js'에서 Express 애플리케이션을 가져옵니다.
-
+const app = require("./app");
 const port = process.env.PORT || 8000;
 
-// 서버 시작
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });

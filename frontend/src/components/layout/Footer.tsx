8ebe882b88f6f6b390cebb7bacc5bd55250090ec
Daisy7942 <zotazoang@gmail.com>
2024-09-01 22:46:43 +0900

    feat: footer반응형

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -4,10 +4,16 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-100 h-[280px] pt-[60px] relative">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between">
-          <div className="w-1/3 pl-8">
+    // footer 전체 컨테이너
+    // 모바일에서는 높이 자동 조정, 태블릿 이상에서는 280px 고정
+    <footer className="bg-gray-100 h-auto md:h-[280px] pt-8 md:pt-[60px] relative">
+      {/* 내용을 감싸는 컨테이너 */}
+      <div className="container mx-auto px-8">
+        {/* 로고, 회사 정보, 메뉴를 포함하는 flex 컨테이너 */}
+        <div className="flex flex-col md:flex-row justify-between">
+          {/* 로고 및 회사 정보 섹션 */}
+          <div className="w-full md:w-1/3 pl-0 md:pl-12 mb-8 md:mb-0">
+            {/* 로고 링크 */}
             <a href="/" className="block mb-4">
               <img
                 src="/images/anpicktest.svg"
@@ -15,14 +21,18 @@ const Footer = () => {
                 className="h-8"
               />
             </a>
+            {/* 회사명 */}
             <p className="text-gray-600 font-semibold mb-1">(주) 앤픽</p>
+            {/* 회사 정보 */}
             <div className="text-m text-gray-600">
               <p>상호: 주식회사 앤픽 | 사업자등록번호: 000-00-000000</p>
               <p>주소: 서울특별시 강남구 역삼로 160</p>
               <p>이메일: contact@annpick.net</p>
             </div>
           </div>
-          <div className="w-2/3 flex justify-end space-x-16 pr-8">
+          {/* 메뉴 섹션 */}
+          <div className="w-full md:w-2/3 flex flex-wrap md:flex-nowrap justify-between md:justify-end space-x-0 md:space-x-16 pr-0 md:pr-8">
+            {/* 앤픽 메뉴 */}
             <div>
               <h3 className="font-semibold mb-4">앤픽</h3>
               <ul className="space-y-2">
@@ -33,6 +43,7 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
+            {/* 고객센터 메뉴 */}
             <div>
               <h3 className="font-semibold mb-4">고객센터</h3>
               <ul className="space-y-2">
@@ -53,6 +64,7 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
+            {/* 서비스 메뉴 */}
             <div>
               <h3 className="font-semibold mb-4">서비스</h3>
               <ul className="space-y-2">
@@ -71,7 +83,9 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="absolute bottom-4 left-0 right-0 text-center">
+      {/* 저작권 정보 */}
+      {/* 모바일에서도 보이도록 수정: position 제거, 하단 패딩 추가 */}
+      <div className="mt-8 md:mt-0 md:absolute md:bottom-4 left-0 right-0 text-center pb-4 md:pb-0">
         <p className="text-sm text-gray-500">
           © {currentYear} 앤픽. All rights reserved.
         </p>

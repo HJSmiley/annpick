72124d8fa81b1f97d76c8e06c4c69af696fd463b
HJSmiley <hyeju518@gmail.com>
2024-09-02 15:55:22 +0900

    fix: 포트 80번으로 변경

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => {
   console.log(`Server is running on ${process.env.BACKEND_URL}`);

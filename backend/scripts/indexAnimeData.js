64ca2b34a6168240ce7e3dc6e22e451306d91867
HJSmiley <hyeju518@gmail.com>
2024-09-06 12:02:42 +0900

    fix: Meilisearch 인덱싱 로직 수정

--- backend/scripts/indexAnimeData.js
+++ backend/scripts/indexAnimeData.js
@@ -1,4 +1,16 @@
-const { indexAnimeData } = require("../src/services/animeService");
-require("../src/models/associations");
+const fs = require("fs");
+const { animeIndex } = require("../src/config/meiliConfig");
 
-indexAnimeData();
+const indexAnimeFromFile = async () => {
+  try {
+    const data = fs.readFileSync("../data/anime_data.json", "utf-8");
+    const animes = JSON.parse(data);
+
+    await animeIndex.addDocuments(animes);
+    console.log("Anime data indexed successfully.");
+  } catch (error) {
+    console.error("Error indexing anime data from file:", error);
+  }
+};
+
+indexAnimeFromFile();

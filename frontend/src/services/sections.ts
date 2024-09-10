3e0fdd7fc1e68a27f218abada0532b69180a3e5c
HJSmiley <hyeju518@gmail.com>
2024-09-10 11:58:05 +0900

    feat: sections.ts에 랜덤 로직 추가

--- frontend/src/services/sections.ts
+++ frontend/src/services/sections.ts
@@ -3,19 +3,31 @@ export interface SectionData {
   ids: number[];
 }
 
+// 주어진 범위 내에서 랜덤한 숫자를 생성하는 함수
+const getRandomIds = (count: number, max: number): number[] => {
+  const ids: number[] = [];
+  while (ids.length < count) {
+    const randomId = Math.floor(Math.random() * max) + 1; // 1부터 max까지 랜덤 숫자 생성
+    if (!ids.includes(randomId)) {
+      ids.push(randomId); // 중복되지 않게 추가
+    }
+  }
+  return ids;
+};
+
 export const getAnimeSections = (): SectionData[] => {
   return [
     {
       title: "마음을 울리는 애니메이션",
-      ids: [1, 14, 2, 13, 6, 3, 7, 8, 9, 10],
+      ids: getRandomIds(10, 3503), // 10개의 랜덤 아이디
     },
     {
       title: "보는 내내 가슴이 두근거리는 애니메이션",
-      ids: [14, 12, 10, 8, 6, 4, 2, 1],
+      ids: getRandomIds(8, 3503), // 8개의 랜덤 아이디
     },
     {
       title: "깊은 철학적 메시지를 담은 애니메이션",
-      ids: [19, 7, 5, 3, 1],
+      ids: getRandomIds(5, 3503), // 5개의 랜덤 아이디
     },
   ];
 };
